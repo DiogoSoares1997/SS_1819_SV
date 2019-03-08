8635051b2fec80f9482d9ca2c94388a0584ca0b8
AndreESA <aesantos.alves@gmail.com>
2019-03-08 11:36:58 +0000

    por a sinc a funcionar

--- TrabalhoPrático/my_sinusoid.m
+++ TrabalhoPrático/my_sinusoid.m
@@ -1,5 +1,6 @@
-function [x,y ] = my_sinusoid( A,f0,t, alpha )
+function [x,y ] = my_sinusoid( A,B,f0,t, alpha )
     x = A * cos((2*pi*f0*t)+alpha);
-    y = (x.^2).*(0.5*t);
+    %y = (x.^2).*(0.5*t);
+    y = (x.^2).*(B*t);
 end
 
--- TrabalhoPrático/typical_signals_test.m
+++ TrabalhoPrático/typical_signals_test.m
@@ -12,7 +12,7 @@ alpha = -pi;
 t = 0:1.2500e-04:0.008;
 
 %[z] = my_sinc(A,B,C,t);
-[x,y ] = my_sinusoid( A,f0,t, alpha );
+[x,y] = my_sinusoid(A,B,f0,t,alpha);
 
 figure (1)
 plot (t,x)
@@ -29,13 +29,13 @@ f0 = 500;
 alpha = 0;
 t = 0:1.2500e-04:0.008;
 
-[x,y] = my_sinusoid(A,f0,t,alpha);
+[x,y] = my_sinusoid(A,B,f0,t,alpha);
 figure (3)
 plot (t,x)
 figure (4)
 plot (t,y)
 
-%% sinal Z exercicio 1 b)
+%% sinal Z exercicio 1 a)
 
 %i)
 
@@ -48,6 +48,7 @@ t = -10:0.1:10;
 
 figure (5)
 plot(t,z)
+grid on
 
 %% sinal Z exercicio 1 b)
 
