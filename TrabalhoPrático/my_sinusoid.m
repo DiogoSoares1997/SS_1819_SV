555b20e9ab1c619106d5d559347db42ed00ec2a8
AndreESA <aesantos.alves@gmail.com>
2019-03-08 12:12:21 +0000

    comeco do ex 2

--- TrabalhoPrático/my_sinusoid.m
+++ TrabalhoPrático/my_sinusoid.m
@@ -1,6 +1,6 @@
 function [x,y ] = my_sinusoid( A,B,f0,t, alpha )
     x = A * cos((2*pi*f0*t)+alpha);
     %y = (x.^2).*(0.5*t);
-    y = (x.^2).*(B*t);
+    y = (A * cos((2*pi*f0*B*t)+alpha)).^2;
 end
 
