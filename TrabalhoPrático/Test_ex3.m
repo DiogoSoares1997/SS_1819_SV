0de4eca01610be037c69dee0b06901390ef346e5
AndreESA <aesantos.alves@gmail.com>
2019-03-18 12:04:37 +0000

    sinal no 3 era pequeno demais logo nao se via

--- TrabalhoPrático/Test_ex3.m
+++ TrabalhoPrático/Test_ex3.m
@@ -3,10 +3,11 @@ close all
 
 %% Exercicio 3 Sistema S
 
-t = -10:0.01:10;
-a = 2.*cos(2*pi*1000*t);
-b= 2.*cos(2*pi*1000*t);
-d = 0 *t;
+t = -0.001:0.00001:0.001;
+
+a = 2*cos(2*pi*1000*t);
+b= 2*cos(2*pi*1000*t);
+d = 0*t;
 
 [c,e]=SystemS(a,b,d);
 figure(1)
